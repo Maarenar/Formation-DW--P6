4cd52ed8f2859ce81aecf56688584af487ec0e77
Marion Renard <mrenard37@gmail.com>
2020-08-14 10:20:43 +0200

    addition of like and dislike function

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -49,49 +49,52 @@ exports.getAllSauce = (req, res, next) => {
      .catch(error => res.status(400).json({error}));
    };
 
-/*
-let usersLiked = [];
-let usersDisliked = [];
+exports.likeSauce = (req, res, next) => {
 
------LIKE------
-exports.likeSauce = clique sur le bouton like
-    si user neutre au sujet de la sauce -> ajoute un like
-    if (!user_id in usersLiked && !user_id in usersDisliked){
-        --> ajoute le user_id dans le tableau usersLiked de cette sauce// usersLiked.push(user_id);
-        --> increment de 1 le number likes de la sauce // likes =+ 1;
-        --> icone like devient coloree
+    switch (req.body.like) {
+      case 0:
+        Sauce.findOne({ _id: req.params.id })
+          .then((sauce) => {
+            if (sauce.usersLiked.find(user => user === req.body.userId)) {
+              Sauce.updateOne({ _id: req.params.id }, {
+                $inc: { likes: -1 },
+                $pull: { usersLiked: req.body.userId },
+                _id: req.params.id
+              })
+                .then(() => { res.status(201).json({ message: 'Ton avis a été pris en compte!' }); })
+                .catch((error) => { res.status(400).json({ error: error }); });
+  
+            } if (sauce.usersDisliked.find(user => user === req.body.userId)) {
+              Sauce.updateOne({ _id: req.params.id }, {
+                $inc: { dislikes: -1 },
+                $pull: { usersDisliked: req.body.userId },
+                _id: req.params.id
+              })
+                .then(() => { res.status(201).json({ message: 'ok...' }); })
+                .catch((error) => { res.status(400).json({ error: error }); });
+            }
+          })
+          .catch((error) => { res.status(404).json({ error: error }); });
+        break;
+      case 1:
+        Sauce.updateOne({ _id: req.params.id }, {
+          $inc: { likes: 1 },
+          $push: { usersLiked: req.body.userId },
+          _id: req.params.id
+        })
+          .then(() => { res.status(201).json({ message: 'Like added!' }); })
+          .catch((error) => { res.status(400).json({ error: error }); });
+        break;
+      case -1:
+        Sauce.updateOne({ _id: req.params.id }, {
+          $inc: { dislikes: 1 },
+          $push: { usersDisliked: req.body.userId },
+          _id: req.params.id
+        })
+          .then(() => { res.status(201).json({ message: 'Ok... it\'\s your right...' }) })
+          .catch((error) => { res.status(400).json({ error: error }) });
+        break;
+      default:
+        console.error('Bad request')
     }
-
-   si user aime deja la sauce -> annule le like
-        --> retire le user_id dans le tableau usersLiked de cette sauce
-        --> decremente de 1 le number likes de la sauce 
-        --> icone like redevient blanche
-
-   si user n'aimait pas la sauce -> annule le dislike et ajoute un like
-        --> ajoute le user_id dans le tableau usersLiked de cette sauce
-        --> retire le user_id du tableau usersDisliked de cette sauce
-        --> increment de 1 le number likes de la sauce
-        --> decrement de -1 de number dislikes de la sauce
-        --> icone like devient coloree, icone dislike redevient blanche 
-
------DISLIKE-----
-exports.dislikeSauce = clique sur le bouton dislike
-
-    si user neutre au sujet de la sauce -> ajoute un dislike
-        --> ajoute le user_id dans le tableau usersDisliked de cette sauce
-        --> increment de -1 le number dislikes de la sauce 
-        --> icone like devient coloree
-
-    si user n'aimait deja pas la sauce -> annule le dislike
-        --> retire le user_id du tableau usersDisliked de cette sauce
-        --> decrement de -1 le tableau usersDisliked de cette sauce
-        --> icone dislike redevient blanche
- 
-
-   si user aime deja la sauce -> annule le like et ajoute un dislike
-        --> retire le user_id dans le tableau usersLiked de cette sauce
-        --> ajoute le user_id dans le tableau usersDisliked de cette sauce
-        --> decremente de 1 le number likes de la sauce 
-        --> incremente de -1 le number de dislikes de la sauce
-        --> icone like redevient blanche, icone dislike devient coloree
-*/
+  };
